0bd34a7da298916d938739cb0e1dcf98ee49f90c
Elwin <elwin.carlos09@gmail.com>
2021-04-20 14:08:51 +0100

    stub out redux store and imports/exports

---
+++ src/redux/stores/Store/index.js
@@ -0,0 +1,9 @@
+  
+import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
+// import reducer from '../../reducers';
+
+const store = createStore(playerReducer, devToolsEnhancer());
+
+export default store;
---
+++ src/redux/stores/index.js
@@ -0,0 +1,1 @@
+export {deafult as store } from './Store';

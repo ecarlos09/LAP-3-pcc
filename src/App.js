a26a963a6b96aebdc2e18e9fb04dea1939af9fd0
mfy <Faisaly1234@outlook.com>
2021-04-20 15:34:57 +0100

    added boilerpplate for pages and components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+
+
+function App() {
+    return (
+        <>
+        </>
+       
+    )
+}
+
+export default App;

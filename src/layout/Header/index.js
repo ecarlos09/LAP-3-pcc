6cd494b56fa3935d35c3e598120bd60fca9c2df2
Elwin <elwin.carlos09@gmail.com>
2021-04-21 09:01:53 +0100

    rezise logo and center heading

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './style.css';
+
 const Header = () => {
     return (
         <header>
-            <i className="fab fa-github"></i>
+            <span id="logo"><i className="fab fa-github"></i></span>
             <h1>Welcome to Github Repo Finder!</h1>
         </header>
     )

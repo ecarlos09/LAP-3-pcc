89a78863a19fd508737f03cda46c49909d2c43d0
Elwin <elwin.carlos09@gmail.com>
2021-04-21 08:51:11 +0100

    move heading to a Header component in layout and add logo

---
+++ src/layout/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <i className="fab fa-github"></i>
+            <h1>Welcome to Github Repo Finder!</h1>
+        </header>
+    )
+}
+
+export default Header;
--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,19 +1,18 @@
-import React, { useState }from 'react'
-import {Form, Repo} from '../../components'
+import React, { useState }from 'react';
+import {Form, Repo} from '../../components';
+import{ Header } from '../../layout';
 
 
 const Homepage = ({username, setUsername}) => {
     const [repoList, setRepoList] = useState([])
-
-
    
     return (
         <>
-        <h1>Home!</h1>
+        <Header />
         <Form setRepoList={setRepoList} username={username} setUsername={setUsername}/>
         <Repo repoList={repoList} setRepoList ={setRepoList}/>
         </>
     )
 }
 
-export default Homepage
+export default Homepage;

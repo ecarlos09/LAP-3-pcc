1a8fa8a915c404bbbc061e4e5c782580cc024984
Elwin <elwin.carlos09@gmail.com>
2021-04-21 07:54:57 +0100

    add back and forward buttons

--- src/layout/NavBar/index.js
+++ src/layout/NavBar/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 
 const NavBar = () => {
+    const history = useHistory();
+
     return (
         <nav>
             <NavLink exact to="/" activeClassName="current">Home</NavLink>
             <NavLink to="/repopage" activeClassName="current">Repo Info</NavLink>
+            <button id="back-button" onClick={history.goBack}>Go back</button>
+            <button id="forward-button" onClick={history.goForward}>Go forward</button>
         </nav>
     )
 }

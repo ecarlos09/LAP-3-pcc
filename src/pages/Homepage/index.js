c78390b4e0c3c5b2c6b90d74466f2c8bf66c2638
Elwin <elwin.carlos09@gmail.com>
2021-04-21 12:10:02 +0100

    add custom error message for user not found

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import {Form, Repo} from '../../components';
 import{ Header } from '../../layout';
 
 
 const Homepage = () => {
+
+    const error = useSelector(state => state.error);
    
     return (
         <>
         <Header />
         <Form />
-        <Repo />
+        { error ? <p role='alert'>Oops!  We cannot find that user.  Try a different name.</p> : <Repo /> }
         </>
     )
 }

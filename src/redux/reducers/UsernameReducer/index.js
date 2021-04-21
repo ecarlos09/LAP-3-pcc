70c65fe43a640a05ba320a07a7b734955193b379
Elwin <elwin.carlos09@gmail.com>
2021-04-21 16:30:47 +0100

    reset error value in store upon succesful action

--- src/redux/reducers/UsernameReducer/index.js
+++ src/redux/reducers/UsernameReducer/index.js
@@ -7,16 +7,18 @@ const initState = {
 
 const usernameReducer = (state=initState, action) => {
     switch(action.type){
+        case "LOADING":
+            return { ...state, username: action.payload, repos: [], repoInfo: {}, loading: true, error: false };
         case "LOAD_REPOS":
             const username = action.payload.username;
             const repoList = action.payload.list;
             const avatar = action.payload.avatar;
-            return { ...state, username: username, repos: repoList, avatar: avatar };
+            return { ...state, username: username, repos: repoList, avatar: avatar, error: false };
         case "LOAD_REPO_INFO":
             const repoInfo = action.payload;
-            return { ... state, repoInfo: repoInfo};
+            return { ... state, repoInfo: repoInfo, error: false };
         case 'SET_ERROR':
-            return { ...state, error: action.payload }
+            return { ...state, error: action.payload, loading: false }
         default:
             return state;
     }
